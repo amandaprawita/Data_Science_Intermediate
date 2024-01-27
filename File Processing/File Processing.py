ec7f5c3b9edfbf24f3250d3a60009ef5178e0aeb
Amanda Prawita Ningrum <103196820+amandaprawita@users.noreply.github.com>
2024-01-27 14:04:06 +0700

    Add files via upload

---
+++ File Processing/File Processing.py
@@ -0,0 +1,6 @@
+file = open ("coding-studio.txt", "a")
+# print(file) "r"
+# print(file.read()) "r"
+# file.write("Ini adalah text yang baru") # "w"
+file.write("\nIni adalah text yang diappend dengan enter") # "a"
+file.close
